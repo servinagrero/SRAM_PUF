cc509099f30538d0dddaf42750ceccee91a2d02c
servinagrero <servinagrero@gmail.com>
2019-09-16 22:46:43 +0200

    Updated comparer script

--- src/comparer.py
+++ src/comparer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+from itertools import combinations
+from pathlib import Path
 # import argparse
 
 # ap = argparse.ArgumentParser(description="Small script to compare two binary files")
 # ap.add_argument("-f", "--file", required=True, help="Path to the first file.")
+# ap.add_argument("-d", "--dir", required=False, help="Directory with the binary files.")
 # args = vars(ap.parse_args())
 
 # TODO: Given a directory with binary files, check all files and permutations
@@ -26,15 +29,20 @@ def compare_files(file1, file2):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) > 3:
         print("Two files to compare must be provided")
         exit(1)
     else:
-        file1 = open(sys.argv[1], "rb").read()
-        file2 = open(sys.argv[2], "rb").read()
-        if not file_is_valid(file1) or not file_is_valid(file2):
-            print("Files must be 1024 bytes long.")
-            exit(1)
-        else:
-            diff = compare_files(file1, file2)
-            print(f'Difference between files is {diff:2.2f}%')
+        path = Path(sys.argv[1])
+        data_files = [f.as_posix() for f in path.iterdir() if f.is_file()]
+        print(data_files)
+        f_combinations = list(combinations(data_files, 2))
+        # file_combinations = combinations(file_list, 2)
+        # file1 = open(sys.argv[1], "rb").read()
+        # file2 = open(sys.argv[2], "rb").read()
+        # if not file_is_valid(file1) or not file_is_valid(file2):
+        #     print("Files must be 1024 bytes long.")
+        #     exit(1)
+        # else:
+        #     diff = compare_files(file1, file2)
+        #     print(f'Difference between files is {diff:2.2f}%')
