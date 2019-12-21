a0df81906b943d0ecb592fceb1c0f1f54b0aaae1
Sergio Vinagrero <servinagrero@gmail.com>
2019-12-21 23:52:07 +0100

    Updated jupyter script to calculate uniformity

--- src/jupyter/sample.py
+++ src/jupyter/sample.py
@@ -4,14 +4,15 @@ from datetime import datetime
 
 
 class Sample(object):
-    '''
+    """
     Sample represents a final dump of data.
     It has the following fields:
       - All the parameters from the board
       - All the data from the dump
       - All the bits from the data dump
       - Timestamp and Datetime from the dump
-    '''
+    """
+
     def __init__(self, sample):
         self.__dict__ = sample
         self._id
@@ -29,8 +30,7 @@ class Sample(object):
         self.Vrefint_cal
         self.Timestamp
         self.Data
-        self.Datetime = datetime.strptime(sample['Timestamp'],
-                                          '%d-%m-%Y-%H:%M:%S')
+        self.Datetime = datetime.strptime(sample["Timestamp"], "%d-%m-%Y-%H:%M:%S")
 
     def __xor__(self, other):
         pass
